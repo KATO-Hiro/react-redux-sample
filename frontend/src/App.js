ee830a4d5eb7a2156300789d976a59d183f0a21f
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-04 23:09:28 +0000

    Enable to save values in local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Redux and Persist </h1>
+        <Message />
+        <Button />
+      </div>
+    );
+  }
 }
+
+App = connect()(App);
+
+class Message extends Component {
+  style = {
+    fontSize: "20pt",
+    padding: "20px 5px"
+  }
+
+  render() {
+    return (
+      <p style={this.style}>
+        {this.props.message}: {this.props.counter}
+      </p>
+    );
+  }
+}
+
+Message = connect((state) => state)(Message);
+
+class Button extends Component {
+  style = {
+    fontSize: "16pt",
+    padding: "5px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doAction(e) {
+    if (e.shiftKey) {
+      this.props.dispatch({ type: 'DECREMENT' });
+    } else if (e.ctrlKey) {
+      this.props.dispatch({ type: 'RESET' });
+    } else {
+      this.props.dispatch({ type: 'INCREMENT' });
+    }
+  }
+
+  render() {
+    return (
+      <button style={this.style} onClick={this.doAction}>click</button>
+    );
+  }
+}
+
+Button = connect()(Button);
 
 export default App;
